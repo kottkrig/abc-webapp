ec9c83e1797dc3aebe4546f2e1421fc2bcaa4615
Johan Larsson <johan@johanl.se>
2012-10-07 13:59:12 +0200

    Removed åäö
    Github doesn't like swedish characters in file names. Have to find
    another solution.

--- javascripts/app.js
+++ javascripts/app.js
@@ -2,7 +2,7 @@ window.onload = init;
 
 var context;
 var bufferLoader;
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'å', 'ä', 'ö', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 function init() {
 
